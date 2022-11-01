4f1ad57fa39b7ca9043f6d93b1464596aef35018
SixtyByte <valentinvodopyan@ukr.net>
2022-11-01 20:21:11 +0200

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(SimpleLightbox)
+
+const gallery= document.querySelector(`.gallery`);
+
+const markup=galleryItems.map(({preview,original,description})=>
+`<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join(``);
+gallery.insertAdjacentHTML(`beforeend`,markup)
+
+new SimpleLightbox('.gallery a', { /* options */ });
+
