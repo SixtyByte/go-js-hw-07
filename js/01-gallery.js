1dfed6666e02969c1bdc9062314fe4b31c235016
SixtyByte <valentinvodopyan@ukr.net>
2022-11-01 19:13:57 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const gallery= document.querySelector(`.gallery`);
+gallery.addEventListener('click',onClickImage);
+
+const markup=galleryItems.map(({preview,original,description})=>
+`<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join(``);
+gallery.insertAdjacentHTML(`beforeend`,markup)
+
+
+
+function onClickImage (event){
+  event.preventDefault();
+const instance = basicLightbox.create(`
+	<img src=${event.target.dataset.source} width="500" height="600">
+`);
+instance.show();
+
+
+}
